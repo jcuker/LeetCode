81e1fd852cb314b3a778fdfb45f7e71db26735ca
Jordan Cuker <eliteforcejac@gmail.com>
2017-11-11 18:30:04 -0600

    Fix for TwoSum in C#

--- TwoSum/C#/TwoSum.cs
+++ TwoSum/C#/TwoSum.cs
@@ -9,39 +9,65 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
+/*
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4) (342 + 465) = 807
+Output: 7 -> 0 -> 8
+*/
 
 using System;
 
 namespace LeetCode
 {
-    public class TwoSumSolution
+    public class AddTwoNumbersSolution
     {
-        public static int[] TwoSum(int[] nums, int target) 
+
+        public static int[] TwoSum(int[] nums, int target)
         {
             int[] returnResult = new int[2];
-            int intCurrentIndex = 1, intPreviousIndex = 0;
 
-            while(intCurrentIndex < nums.Length)
+            int intStartingIndex = 0, intCurrentIndex = 0;
+
+            while (intStartingIndex < nums.Length)
             {
-                if(nums[intPreviousIndex] + nums[intCurrentIndex] == target)
-                {
-                    returnResult[0] = intPreviousIndex;
-                    returnResult[1] = intCurrentIndex;
-                    return returnResult;
-                }
-                intPreviousIndex = intCurrentIndex;
+                intCurrentIndex = intStartingIndex + 1;
 
-                intCurrentIndex++;
+                while(intCurrentIndex < nums.Length)
+                {
+
+                    if (nums[intCurrentIndex] + nums[intStartingIndex] == target)
+                    {
+                        if(intCurrentIndex < intStartingIndex)
+                        {
+                            returnResult[0] = intCurrentIndex;
+                            returnResult[1] = intStartingIndex;
+                        }
+                        else
+                        {
+                            returnResult[0] = intStartingIndex;
+                            returnResult[1] = intCurrentIndex;
+                        }
+                        return returnResult;
+                    }
+
+                    intCurrentIndex++;
+                }
+
+                intStartingIndex++;
             }
+
             return returnResult;
         }
 
+
         public static void Main()
         {
-            int[] nums = {2, 7, 11, 15};
-            int[] result = TwoSum(nums, 9);
+            int[] nums = { 3, 2, 4 };
+            int[] result = TwoSum(nums, 6);
             Console.WriteLine("[{0}, {1}]", result[0], result[1]);
         }
     }
 }
-
